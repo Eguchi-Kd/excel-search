b0db6243d363a5db4e589bd2f594ab9003a104ab
Eguchi-Kd <student7653@gmail.com>
2024-10-20 23:47:21 +0900

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import os
 def test():
   print("テステス")
-  return print(os.environ.get("TEST_ID"))#render_template(index.html,TEST_ID=os.environ.get("TEST_ID"))
+  return print(os.environ.get("VITE_TEST"))#render_template(index.html,TEST_ID=os.environ.get("VITE_TEST"))
 test()
