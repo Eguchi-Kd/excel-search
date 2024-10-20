a8ac60a449b6d511959ad28bc83d317a9981ef70
Eguchi-Kd <student7653@gmail.com>
2024-10-20 19:05:36 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   define: {
-    'process.env.TEST_ID': JSON.stringify(process.env.TEST_ID)
+    'process.env.TEST_ID': JSON.stringify(process.env.VITE_TEST_ID)
   }
 });
