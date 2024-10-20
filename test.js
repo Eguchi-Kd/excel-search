9a42b14c2e41d02fd66005d3ca6a6101c5767d1d
Eguchi-Kd <student7653@gmail.com>
2024-10-20 23:50:06 +0900

    Update test.js

--- test.js
+++ test.js
@@ -1,5 +1,5 @@
 console.log("オケイ");
 console.log(import.meta.env);
-const test_key = import.meta.env.VITE_TEST;
+const test_key = import.meta.env.get("VITE_TEST");
 // require('dotenv').config();
 // console.log(process.env.TEST_ID)
