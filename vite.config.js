9fa0c9be1c21d73dc756465ab8fe059baf427df5
Eguchi-Kd <student7653@gmail.com>
2024-10-20 22:44:42 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
 import { defineConfig } from 'vite';
-export default defineConfig({
-  //envDir:'../'
+export default defineConfig(({ command, mode }) => {
+  // Load env file based on `mode` in the current working directory.
+  // Set the third parameter to '' to load all env regardless of the `VITE_` prefix.
+  const env = loadEnv(mode, process.cwd(), '')
+  return {
+    // vite config
+    define: {
+      __APP_ENV__: JSON.stringify(env.APP_ENV),
+    },
+  }
 });
